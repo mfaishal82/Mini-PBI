dc84783798cb990a4e0d538b4a0d53b4889c3a4a
mfaishal82 <mfaishal82>
2025-05-29 07:16:27 +0700

    day 2

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,5 +1,16 @@
 const projects = [
     {
+        title: "The Fast News",
+        description: "A web-based online-media digital news paper app for the final project assignment of Nusacodes",
+        url: "https://github.com/mfaishal82/media-online",
+        date: "December 2024",
+        image: ["https://res.cloudinary.com/dt1e1fd6s/image/upload/v1736928340/portfolio/rnmcghjyuipfre93pyex.png", "https://res.cloudinary.com/dt1e1fd6s/image/upload/v1736928338/portfolio/zew38z9dz8hdmmoojljh.png"],
+        techStack: "Laravel, PHP, MySQL, HTML, CSS, Bootstrap, AlpineJS",
+        framework: "laravel",
+        slug: "the-fast-news",
+        type: "web"
+    },
+    {
         title: "The Library",
         description: "A simple web-based library information system app",
         url: "https://github.com/mfaishal82/Sistem-Informasi-Perpustakaan",
@@ -8,6 +19,28 @@ const projects = [
         techStack: "PHP, MySQL, HTML, Tailwind, AlpineJS",
         framework: "laravel",
         slug: "the-library",
+        type: "web"
+    },
+    {
+        title: "Tasmi' App",
+        description: "A simple mobile app for listening Qur'an recitation and prayer time schedule",
+        url: "https://github.com/mfaishal82/murattal-quran",
+        date: "July 2024",
+        image: ["https://res.cloudinary.com/dt1e1fd6s/image/upload/v1736928332/portfolio/oun76dadjvzw7jtnclkl.png"],
+        techStack: "React-Native, Expo, Expo location, Axios, Mp3Quran API, Aladhan API",
+        framework: "react",
+        slug: "tasmi-app",
+        type: "mobile"
+    },
+    {
+        title: "Mini Project",
+        description: "Mini Coding Live Project Assignment. A simple web-based project with responsive design",
+        url: "https://github.com/revou-fundamental-course/27-may-24-mfaishal82",
+        date: "June 2024",
+        image: ["https://res.cloudinary.com/dt1e1fd6s/image/upload/v1736928344/portfolio/yswpwe8oxofyzwmrwfr7.png", "https://res.cloudinary.com/dt1e1fd6s/image/upload/v1736928336/portfolio/sbsepkp9hnaeca0mesgu.png"],
+        techStack: "Semantic HTML, CSS, JavaScript",
+        framework: "html",
+        slug: "mini-project",
         type: "web"
     },
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,67 @@ new TypeIt("#myElement", {
 
 function renderProjects() {
   const container = document.getElementById("projectsContainer");
-  if (!container || typeof projects === 'undefined') return;
-  container.innerHTML = projects.map(project => `
-    <div class="project-card">
-      <img class="project-image" src="${project.image[0]}" alt="${project.title}">
-      <div class="project-content">
-        <h3 class="project-title">${project.title}</h3>
-        <div class="project-meta">
-          <span>${project.framework}</span>
-          <span>${project.date}</span>
-        </div>
-        <div class="project-tech">${project.techStack}</div>
-        <p class="project-description">${project.description}</p>
-        <a class="project-link" href="${project.url}" target="_blank">
-          View on my Github -> </i>
-        </a>
-      </div>
-    </div>
-  `).join("");
+
+  projects.forEach((project) => {
+    const slide = document.createElement("div");
+    slide.className = "swiper-slide";
+
+    slide.innerHTML = `
+                    <img src="${project.image[0]}" alt="${
+      project.title
+    }" class="project-image">
+                    <div class="project-content">
+                        <h3 class="project-title">${project.title}</h3>
+                        <div class="project-meta">
+                            <span>${project.date}</span>
+                            <span class="project-type">${project.type}</span>
+                        </div>
+                        <p class="project-description">${
+                          project.description
+                        }</p>
+                        <div class="project-tech">${project.techStack}</div>
+                        <a href="${
+                          project.url
+                        }" target="_blank" class="project-link">
+                            <i class="fab fa-github"></i>
+                            View on my GitHub
+                        </a>
+                    </div>
+                `;
+
+    container.appendChild(slide);
+  });
 }
+
+function initSwiper() {
+  new Swiper(".projectSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+}
+
 renderProjects();
+initSwiper();
